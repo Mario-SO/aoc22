482c1660329ce4a5e977261a21e96a5ebd818e88
Mario S.O <gh_mario.proclaim@slmail.me>
2022-12-06 15:41:55 -0500

    ✅ Day 4 part 1

---
+++ d4/solution.py
@@ -0,0 +1,24 @@
+import sys
+
+file = open("input.txt", "r")
+
+def part1():
+
+    fully_contained = 0
+
+    for line in file:
+        array1, array2 = line.split(",")
+
+        array1 = list(range(int(array1.split("-")[0]), int(array1.split("-")[1]) + 1))
+        array2 = list(range(int(array2.split("-")[0]), int(array2.split("-")[1]) + 1))
+
+        if set(array1).issubset(array2) or set(array2).issubset(array1):
+            fully_contained += 1
+
+    print(fully_contained)
+
+def part2():
+
+if __name__ == '__main__':
+    part1()
+    part2()
